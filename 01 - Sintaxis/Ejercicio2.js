90488947f2d216e3326fecdade78e333a0080647
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-07-27 13:02:45 -0500

    Ejercicio 2 - Sintaxis

---
+++ 01 - Sintaxis/Ejercicio2.js
@@ -0,0 +1,14 @@
+const lista = [
+  "Arnold",
+  30,
+  true,
+  new Date(1992, 6, 12),
+  {
+    titulo: "Fundation",
+    autor: "Isaac Asimov",
+    fecha: 1991,
+    url: "https://www.amazon.com/-/es/Isaac-Asimov/dp/0553293354",
+  },
+];
+
+console.log(lista);
