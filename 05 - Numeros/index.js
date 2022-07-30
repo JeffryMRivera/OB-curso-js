672a648a19999eabb02ec8e01be93affe5aecb0f
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-07-30 10:45:51 -0500

    Ejercicio 5

---
+++ 05 - Numeros/index.js
@@ -0,0 +1,6 @@
+let alturaCms = 183;
+let alturaMts = 1.83;
+let pesoKg = 85.3;
+let alturaUp = Math.round(alturaMts);
+let pesoDown = Math.floor(pesoKg);
+let comparacion = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
--- index.js
+++ index.js
@@ -1,40 +1,81 @@
 console.log("Hola Mundo");
 
-/*
-// Cantidad de caracteres
-length;
+/* Generales
+    // Convertir a string
+    toString()
+    // Obtener tipo de una variable
+    typeof x
+*/
 
-// obtener una porcion del string
-slice()
-substring()
+/* String
+    // Cantidad de caracteres
+    length;
 
-//expresiones regulares regexr.com
-/palabra/g
-// buscar todas las coincidencias en un string
-match(/palabra/g);
-// buscar una palabra en el string
-includes();
-// El texto inicia o termina con
-startsWith()
-endsWith()
+    // obtener una porcion del string
+    slice()
+    substring()
 
-// reemplazar texto
-replace() 
+    //expresiones regulares regexr.com
+    /palabra/g
+    // buscar todas las coincidencias en un string
+    match(/palabra/g);
+    // buscar una palabra en el string
+    includes();
+    // El texto inicia o termina con
+    startsWith()
+    endsWith()
 
-// concatenar textos
-concat()
+    // reemplazar texto
+    replace() 
 
-// Convertir a mayusculas y minusculas
-toLowercase();
-toUppercase();
+    // concatenar textos
+    concat()
 
-//limpiar inicio y fin del string
-trim();
-trimStart();
-trimEnd();
+    // Convertir a mayusculas y minusculas
+    toLowercase();
+    toUppercase();
 
-// Obtener una posición de un caracter o un string
-charAt();
-indexOf();
-lastIndexOf();
+    //limpiar inicio y fin del string
+    trim();
+    trimStart();
+    trimEnd();
+
+    // Obtener una posición de un caracter o un string
+    charAt();
+    indexOf();
+    lastIndexOf();
+*/
+
+/* Numeros
+    // Obtener valores reales con decimales
+    Math.round((0.1 + 0.2)*100/100)
+ 
+    // Convertir a string limitando el número de decimales en x
+    toFixed(caracteres)
+    
+    // Convierte a string Limitando el número de cifras significativas
+    toPresicion()
+
+    // Obtener valores númericos a partir de literales
+    valueOf()
+
+    // NaN - Not a Number - Infinity
+    let a = new Number("adios")
+    isNaN(a)
+
+    // Convertir a entero
+    parseInt()
+    // Convertir a decimal con base
+    parseInt('0x3a5b7', 16)
+    // Convertir a Decimal
+    parseFloat()
+
+    Valores minimos y maximos en js
+    let minima_presicion = Number.EPSILON;
+    let minimo_valor = Number.MIN_VALUE;
+    let maximo_valor = Number.MAX_VALUE;
+    
+     
+
+
 */
