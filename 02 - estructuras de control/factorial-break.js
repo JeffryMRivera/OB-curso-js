0685722ee939db8e75d90e6ae2a81c992b8a537d
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-07-30 09:31:11 -0500

    Ejercicio 3

---
+++ 02 - estructuras de control/factorial-break.js
@@ -0,0 +1,8 @@
+let total = 1;
+let cantidad = 10;
+while (true) {
+  total = total * cantidad;
+  cantidad--;
+  if (cantidad === 1) break;
+}
+console.log(total);
---
+++ 02 - estructuras de control/factorial-while.js
@@ -0,0 +1,7 @@
+let total = 1;
+let cantidad = 10;
+while (cantidad > 1) {
+  total = total * cantidad;
+  cantidad--;
+}
+console.log(total);
