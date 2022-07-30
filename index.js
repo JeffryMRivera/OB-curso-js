23ae0147b8846dda10a6f1a2fd387de56d264a01
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-07-30 10:11:45 -0500

    Ejercicio 4

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 console.log("Hola Mundo");
+
+/*
+// Cantidad de caracteres
+length;
+
+// obtener una porcion del string
+slice()
+substring()
+
+//expresiones regulares regexr.com
+/palabra/g
+// buscar todas las coincidencias en un string
+match(/palabra/g);
+// buscar una palabra en el string
+includes();
+// El texto inicia o termina con
+startsWith()
+endsWith()
+
+// reemplazar texto
+replace() 
+
+// concatenar textos
+concat()
+
+// Convertir a mayusculas y minusculas
+toLowercase();
+toUppercase();
+
+//limpiar inicio y fin del string
+trim();
+trimStart();
+trimEnd();
+
+// Obtener una posición de un caracter o un string
+charAt();
+indexOf();
+lastIndexOf();
+*/
