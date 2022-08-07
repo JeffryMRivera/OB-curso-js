d1820af4eaf89db3206705c3a31afe2aa964111d
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-08-06 20:09:54 -0500

    Ejercicio 12

---
+++ 12 - Debug/index.js
@@ -0,0 +1,14 @@
+function secuencia(num) {
+  let i = 2;
+  let lista = [1, 1];
+  do {
+    if (num == 1) return [1];
+    if (num == 2) return [1, 1];
+    lista = [...lista, lista[i - 1] + lista[i - 2]];
+    i++;
+  } while (i < num);
+
+  return lista;
+}
+
+console.log(secuencia(6));
